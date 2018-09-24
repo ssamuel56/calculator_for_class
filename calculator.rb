11f3e04b17a47b64c997754166b3b3c665d846b6
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-09-24 16:47:03 -0400

    Made all my tests work

--- calculator.rb
+++ calculator.rb
@@ -9,3 +9,42 @@ end
 def divide(num1, num2)
   num1 / num2
 end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts "Simple calculator!"
+print ">>"
+
+# numbers = $stdin.gets.chomp.split(" ")
+
+# puts numbers
+
+# num1 = numbers[0].to_f
+# num2 = numbers[2].to_f
+# opp = numbers[1].to_s
+
+# puts opp.class
+
+def calculate(num1, opp, num2)
+  if opp == "+"
+    return add(num1, num2)
+  elsif opp == "-"
+    return subtract(num1, num2)
+  elsif opp == "*"
+    return multiply(num1, num2)
+  elsif opp == "/"
+    if num1 == 0 || num2 == 0
+      puts "Please don't use a 0"
+      load 'calculator.rb'
+    else
+      return divide(num1, num2)
+    end
+  else
+    puts "Only simple math please"
+    load 'calculator.rb'
+  end
+end
+
+# calculate(num1, opp, num2)
--- calculator_tdd.rb
+++ calculator_tdd.rb
@@ -1,25 +1,19 @@
-require_relative 'calculator.rb'
 require 'minitest/autorun'
+require_relative 'calculator.rb'
 
-class Add_test < Minitest::Test
+class All_test < Minitest::Test
   def test_add
     assert_equal(12, add(6, 6))
   end
-end
-
-class Sub_test < Minitest::Test
+  def test_calculate
+    assert_equal(100, calculate(50, "+", 50))
+  end
   def test_subtract
     assert_equal(15, subtract(20, 5))
   end
-end
-
-class Div_test < Minitest::Test
   def test_divide
     assert_equal(10, divide(20, 2))
   end
-end
-
-class Multipy_test < Minitest::Test
   def test_multiply
     assert_equal(54, multiply(27, 2))
   end
