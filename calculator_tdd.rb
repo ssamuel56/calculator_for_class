b648591380bbc185275786c225cdafe759f13428
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-09-24 15:54:19 -0400

    Did my test for subtract

--- calculator_tdd.rb
+++ calculator_tdd.rb
@@ -2,13 +2,13 @@ require_relative 'calculator.rb'
 require 'minitest/autorun'
 
 class Add_test < Minitest::Test
-  def test_1
+  def test_add
     assert_equal(14, add(6, 6))
   end
 end
 
-class Subb_test < Minitest::Test
-  def testing_subtract
-    assert_equal(15 subtract(20, 5))
+class Sub_test < Minitest::Test
+  def test_subtract
+    assert_equal(15, subtract(20, 5))
   end
 end
